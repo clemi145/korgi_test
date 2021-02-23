6cc5be07a1f651a9154ddcf450912553d07603ce
Jakob <jakob.fischer0408@gmail.com>
2021-02-23 16:37:16 +0100

    Statistics (TODO)

---
+++ resources/js/Pages/LineChart.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script>
+import {Line} from "vue-chart.js";
+export default {
+    name: "Statistics2",
+    extends:Line,
+    props:{
+        label:{
+            type: String
+        },
+        chartData:{
+            type: Array
+        },
+        options:{
+            type:Object
+        }
+    },
+    mounted() {
+        const dates =this.chartData.map(d=>d.date).reverse();
+        const totals=this.chartData.map(d=>d.total).reverse();
+        this.renderChart({
+                labels:dates,
+                datasets:[{
+                    label:this.label,
+                    data:totals,
+
+                }],
+            },
+            this.options);
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
