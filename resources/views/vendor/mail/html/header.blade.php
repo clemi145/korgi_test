1c5f25088bb05224b50c843f3a2f3343da5a442b
Jakob <jakob.fischer0408@gmail.com>
2021-03-09 12:24:36 +0100

    Styling: E-Mail Styling Test

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Korgi')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="/images/korgi_full_l.svg" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
